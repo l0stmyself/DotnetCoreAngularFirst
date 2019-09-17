a899b7541c8f51abc7fc38678ccd2d9f074c384f
Varun Suresh <varun.suresh@aparinnosys.com>
2019-09-17 17:55:46 +0530

    End of Section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -21,7 +21,7 @@ namespace DatingApp.API.Controllers
             _context = context;
         }
 
-        // GET api/values
+        // GET api/values        
         [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
@@ -31,6 +31,7 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
