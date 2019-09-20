6c225d03109b899d21a5a6b20626c7ea9e53ff61
Varun Suresh <varun.suresh@aparinnosys.com>
2019-09-20 16:20:00 +0530

    End of Section 11

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,40 +1,48 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { environment } from 'src/environments/environment';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
   baseUrl = environment.apiUrl + 'auth/';
   jwtHelper = new JwtHelperService();
   decodedToken: any;
+  currentUser: User;
+  photoUrl = new BehaviorSubject<string>('../../assets/user.png');
+  currentPhotoUrl = this.photoUrl.asObservable();
 
-constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-login(model: any) {
-  return this.http.post(this.baseUrl + 'login', model)
-    .pipe(
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'login', model).pipe(
       map((response: any) => {
-        const user = response;
-        if (user) {
-          localStorage.setItem('token', user.token);
-          this.decodedToken = this.jwtHelper.decodeToken(user.token);
+        if (response) {
+          localStorage.setItem('token', response.token);
+          localStorage.setItem('user', JSON.stringify(response.user));
+          this.currentUser = response.user;
+          this.decodedToken = this.jwtHelper.decodeToken(response.token);
+          this.changeMemberPhoto(this.currentUser.photoUrl);
         }
       })
     );
-}
+  }
 
-register(model: any) {
-  return this.http.post(this.baseUrl + 'register', model);
-}
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model);
+  }
 
-loggedIn() {
-  const token = localStorage.getItem('token');
-  return !this.jwtHelper.isTokenExpired(token);
-}
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
+  }
 
+  changeMemberPhoto(photoUrl: string) {
+    this.photoUrl.next(photoUrl);
+  }
 }
